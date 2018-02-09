660273668fc1b3bc592c9d1390c43f69235ff0be
asuki <asukiaaa@gmail.com>
2018-02-09 14:51:16 +0900

    Get canny edge

--- README.md
+++ README.md
@@ -20,3 +20,4 @@ cargo run
 # References
 - [rscamとrelmを利用して、カメラの画像を表示するアプリをrustで作る方法](http://asukiaaa.blogspot.com/2018/01/rscamrelmrust.html)
 - [asukiaaa/rust_relm_and_camera_practice](https://github.com/asukiaaa/rust_relm_and_camera_practice)
+- [Canny Tutorial](http://www.pages.drexel.edu/~nk752/cannyTut2.html)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate futures_glib;
 extern crate gtk;
 extern crate gdk_pixbuf;
+#[macro_use(s)]
 extern crate ndarray;
 #[macro_use]
 extern crate relm;
@@ -24,9 +25,10 @@ use gtk::{
     WindowType,
 };
 use gtk::Orientation::Vertical;
-use ndarray::{Array, Array2};
+use ndarray::{Array, Array2, arr2};
 use relm::{Relm, Update, Widget};
 use rscam::{Camera, Config};
+use std::ops::Mul;
 use std::time::Duration;
 
 struct Model {
@@ -57,8 +59,8 @@ fn jpeg_vec_to_pixbuf(jpeg_vec: &[u8]) -> Pixbuf {
     loader.get_pixbuf().unwrap()
 }
 
-fn pixbuf_to_gray_mat(color_pixbuf: &Pixbuf) -> Array2<u8> {
-    let mut gray_pixels: Vec<u8> = vec![];
+fn pixbuf_to_gray_mat(color_pixbuf: &Pixbuf) -> Array2<f32> {
+    let mut gray_pixels: Vec<f32> = vec![];
     unsafe {
         for rgb in color_pixbuf.get_pixels().chunks(3) {
             let mut pixel: u16 = (0.3 * rgb[0] as f32 +
@@ -67,31 +69,150 @@ fn pixbuf_to_gray_mat(color_pixbuf: &Pixbuf) -> Array2<u8> {
             if pixel > 0xff as u16 {
                 pixel = 0xff as u16;
             }
-            gray_pixels.push(pixel as u8);
+            gray_pixels.push(pixel as f32);
         }
     }
     let w = color_pixbuf.get_width() as usize;
     let h = color_pixbuf.get_height() as usize;
-    Array::from_vec(gray_pixels).into_shape((w, h)).unwrap()
+    Array::from_vec(gray_pixels).into_shape((h, w)).unwrap()
 }
 
-fn get_edge_mat(gray_mat: Array2<u8>) -> Array2<u8> {
-    gray_mat
+fn apply_gaussian_filter(gray_mat: Array2<f32>) -> Array2<f32> {
+    let mut blur_vec: Vec<f32> = vec![];
+    let edge_detect_mat = arr2(
+        &[[2.,  4.,  5.,  4., 2.],
+          [4.,  9., 12.,  9., 4.],
+          [5., 12., 15., 12., 5.],
+          [4.,  9., 12.,  9., 4.],
+          [2.,  4.,  5.,  4., 2.]]);
+    let base = edge_detect_mat.scalar_sum();
+    let w: usize;
+    let h: usize;
+    {
+        let shape = gray_mat.shape();
+        h = shape[0];
+        w = shape[1];
+    }
+    for i in 0..h {
+        for j in 0..w {
+            if i<3 || h-4<i || j<3 || w-4<j {
+                blur_vec.push(*gray_mat.get((i, j)).unwrap());
+            } else {
+                let mat = gray_mat.slice(s![i-2..i+3, j-2..j+3]);
+                let mat = mat.mul(&edge_detect_mat);
+                let pixel = mat.scalar_sum() / base;
+                blur_vec.push(pixel);
+            }
+        }
+    }
+    Array::from_vec(blur_vec).into_shape((h, w)).unwrap()
 }
 
-fn mat_to_pixbuf(edge_mat: Array2<u8>) -> Pixbuf {
+fn get_rough_angle(angle: f32) -> i32 {
+    if ((angle > 22.5) && (angle < 67.5)) || ((angle < -112.5) && (angle > -157.5)) {
+        45
+    } else if ((angle > 67.5) && (angle < 112.5)) || ((angle < -67.5) && (angle > -112.5)) {
+        90
+    } else if ((angle > 112.5) && (angle < 157.5)) || ((angle < -22.5) && (angle > -67.5)) {
+        135
+    } else {
+        0
+    }
+}
+
+fn is_edge_pixel(current_x: usize, current_y: usize, image_w: usize, strength_vec: &Vec<f32>, compare_angle: i32) -> bool {
+    let current_index = image_w * current_y + current_x;
+    let compare_index1: usize;
+    let compare_index2: usize;
+    match compare_angle {
+        45 => {
+            compare_index1 = image_w * current_y + current_x + 1;
+            compare_index2 = image_w * current_y + current_x - 1;
+        },
+        90 => {
+            compare_index1 = image_w * (current_y - 1) + current_x - 1;
+            compare_index2 = image_w * (current_y + 1) + current_x + 1;
+        },
+        135 => {
+            compare_index1 = image_w * (current_y - 1) + current_x - 1;
+            compare_index2 = image_w * (current_y + 1) + current_x + 1;
+        },
+        _ => {
+            compare_index1 = image_w * (current_y - 1) + current_x;
+            compare_index2 = image_w * (current_y + 1) + current_x;
+        },
+    }
+    let current_strength = strength_vec[current_index];
+    let compare_strength1 = strength_vec[compare_index1];
+    let compare_strength2 = strength_vec[compare_index2];
+    current_strength > compare_strength1 && current_strength > compare_strength2
+}
+
+fn get_edge(gray_mat: Array2<f32>) -> Array2<f32> {
+    let mut edge_vec: Vec<f32> = vec![];
+    let w: usize;
+    let h: usize;
+    {
+        let shape = gray_mat.shape();
+        h = shape[0];
+        w = shape[1];
+    }
+    let gx_mat = arr2(&[[-1.,  0.,  1.],
+                        [-2.,  0.,  2.],
+                        [-1.,  0.,  1.]]);
+    let gy_mat = arr2(&[[-1., -2., -1.],
+                        [ 0.,  0.,  0.],
+                        [ 1.,  2.,  1.]]);
+    let mut strength_vec: Vec<f32> = vec![];
+    let mut angle_vec: Vec<i32> = vec![];
+    for i in 0..h {
+        for j in 0..w {
+            if i<2 || h-3<i || j<2 || w-3<j {
+                strength_vec.push(0.);
+                angle_vec.push(0);
+            } else {
+                let mat = gray_mat.slice(s![i-1..i+2, j-1..j+2]);
+                let gx = mat.mul(&gx_mat).scalar_sum();
+                let gy = mat.mul(&gy_mat).scalar_sum();
+                let strength = (gx.powf(2.) + gy.powf(2.)).sqrt();
+                strength_vec.push(strength);
+                let raw_angle = ((gx/gy).atan() / std::f32::consts::PI) * 180.;
+                angle_vec.push(get_rough_angle(raw_angle));
+            }
+        }
+    }
+    for i in 0..h {
+        for j in 0..w {
+            if i<2 || h-3<i || j<2 || w-3<j {
+                edge_vec.push(0.);
+            } else {
+                let this_index = w * i + j;
+                let result =
+                    if strength_vec[this_index] > 10. && is_edge_pixel(j, i, w, &strength_vec, angle_vec[this_index]) {
+                        255.
+                    } else {
+                        0.
+                    };
+                edge_vec.push(result);
+            }
+        }
+    }
+    Array::from_vec(edge_vec).into_shape((h, w)).unwrap()
+}
+
+fn mat_to_pixbuf(edge_mat: Array2<f32>) -> Pixbuf {
     let uw: usize;
     let uh: usize;
     {
         let shape = &edge_mat.shape();
-        uw = shape[0];
-        uh = shape[1];
+        uh = shape[0];
+        uw = shape[1];
     }
     let iw = uw as i32;
     let ih = uh as i32;
     let mut gray_rgb_vec:Vec<u8> = vec![];
     for p in edge_mat.into_shape((uw * uh)).unwrap().to_vec() {
-        gray_rgb_vec.extend_from_slice(&[p, p, p])
+        gray_rgb_vec.extend_from_slice(&[p as u8, p as u8, p as u8])
     }
     Pixbuf::new_from_vec(
         gray_rgb_vec,
@@ -133,7 +254,8 @@ impl Update for Win {
                         let color_pixbuf = color_pixbuf.unwrap();
                         self.set_msg_timeout(10, Msg::UpdateCameraImage);
                         let gray_mat = pixbuf_to_gray_mat(&color_pixbuf);
-                        let edge_mat = get_edge_mat(gray_mat);
+                        let blur_gray_mat = apply_gaussian_filter(gray_mat);
+                        let edge_mat = get_edge(blur_gray_mat);
                         let pixbuf = mat_to_pixbuf(edge_mat);
                         let gray_image = &self.gray_image;
                         gray_image.set_from_pixbuf(&pixbuf);
